61cacfa1b2a1203cf2019ac24d0066d71cf6ed2e
isaia <isaiahnoelsalazar474@gmail.com>
2025-09-29 18:41:24 +0800

    test: renamed "more-stories" to "more-content"

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center justify-between w-full">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            A Blog Website.
+            Isaiah Noel P. Salazar.
           </h3>
           <a
             href="https://isaiahnoelsalazar.github.io/my-website/"
--- src/app/_components/more-content.tsx
+++ src/app/_components/more-content.tsx
@@ -5,11 +5,11 @@ type Props = {
   posts: Post[];
 };
 
-export function MoreStories({ posts }: Props) {
+export function MoreContent({ posts }: Props) {
   return (
     <section>
       <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
-        More Stories
+        More Content
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
         {posts.map((post) => (

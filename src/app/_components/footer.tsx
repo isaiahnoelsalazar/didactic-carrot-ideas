25a23caf8c3b82a9674178ecb528885b9453e655
isaia <isaiahnoelsalazar474@gmail.com>
2025-09-29 11:59:06 +0800

    test: changed footer elements

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+            A Blog Website.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             {/* <a
@@ -22,6 +22,12 @@ export function Footer() {
             >
               View on GitHub
             </a> */}
+            <a
+              href={"https://isaiahnoelsalazar.github.io/my-website/"}
+              className="mx-3 font-bold hover:underline"
+            >
+              View more projects
+            </a>
           </div>
         </div>
       </Container>

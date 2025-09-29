e4211746d91b71f5c121e736122fbb1264f54a92
isaia <isaiahnoelsalazar474@gmail.com>
2025-09-29 18:28:36 +0800

    test: changed footer design

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200 dark:bg-slate-800">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
+        <div className="py-28 flex flex-col lg:flex-row items-center justify-between">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             A Blog Website.
           </h3>
@@ -22,21 +22,6 @@ export function Footer() {
             >
               View on GitHub
             </a> */}
-            <a
-              className="mx-3 font-bold hover:underline opacity-0 cursor-default"
-            >
-              placeholder :D
-            </a>
-            <a
-              className="mx-3 font-bold hover:underline opacity-0 cursor-default"
-            >
-              placeholder :D
-            </a>
-            <a
-              className="mx-3 font-bold hover:underline opacity-0 cursor-default"
-            >
-              placeholder :D
-            </a>
             <a
               href="https://isaiahnoelsalazar.github.io/my-website/"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"

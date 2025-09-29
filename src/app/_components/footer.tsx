4710b1e677530a5d0299b7b1cec9f0b4c7aaeb34
isaia <isaiahnoelsalazar474@gmail.com>
2025-09-29 18:25:59 +0800

    test: duplicated more elements in footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -33,6 +33,11 @@ export function Footer() {
               placeholder :D
             </a>
             <a
+              className="mx-3 font-bold hover:underline opacity-0 cursor-default"
+            >
+              placeholder :D
+            </a>
+            <a
               href="https://isaiahnoelsalazar.github.io/my-website/"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >

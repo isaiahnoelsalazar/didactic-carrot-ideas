64321260752b00b6daf9120db25914d674bd874c
isaia <isaiahnoelsalazar474@gmail.com>
2025-09-29 18:31:24 +0800

    test: changed footer design

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200 dark:bg-slate-800">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center justify-between">
+        <div className="py-28 flex flex-col lg:flex-row items-center justify-between w-full">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             A Blog Website.
           </h3>

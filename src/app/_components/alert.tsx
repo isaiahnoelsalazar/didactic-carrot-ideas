33aa11caefc134148de122697fe175eb35f25dbc
isaia <isaiahnoelsalazar474@gmail.com>
2025-09-29 18:50:52 +0800

    test: fixed banner layout

--- src/app/_components/alert.tsx
+++ src/app/_components/alert.tsx
@@ -15,7 +15,7 @@ const Alert = ({ preview }: Props) => {
       })}
     >
       <Container>
-        <div className="py-2 text-center text-sm">
+        {/* <div className="py-2 text-center text-sm">
           {preview ? (
             <>
               This page is a preview.{" "}
@@ -29,17 +29,17 @@ const Alert = ({ preview }: Props) => {
             </>
           ) : (
             <>
-              {/* The source code for this blog is{" "}
+              The source code for this blog is{" "}
               <a
                 href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
                 className="underline hover:text-blue-600 duration-200 transition-colors"
               >
                 available on GitHub
               </a>
-              . */}
+              .
             </>
           )}
-        </div>
+        </div> */}
       </Container>
     </div>
   );

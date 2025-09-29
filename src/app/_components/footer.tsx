fe8f79dae0ebcf4f16be41830e9ef18c4aa98ee6
isaia <isaiahnoelsalazar474@gmail.com>
2025-09-29 12:11:01 +0800

    test: changed footer design

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -9,7 +9,7 @@ export function Footer() {
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             A Blog Website.
           </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-end lg:pl-4 lg:w-1/2">
+          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             {/* <a
               href="https://nextjs.org/docs/app/building-your-application/routing/layouts-and-templates"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
@@ -22,6 +22,12 @@ export function Footer() {
             >
               View on GitHub
             </a> */}
+            <a
+              style="opacity: 0;"
+              className="mx-3 font-bold hover:underline"
+            >
+              placeholder :D
+            </a>
             <a
               href="https://isaiahnoelsalazar.github.io/my-website/"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
